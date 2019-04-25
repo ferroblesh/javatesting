9f9cd745f8be376f25783708e2aac0e6b78bd3e4
ferroblesh <ferroblesh@gmail.com>
2019-04-24 23:35:11 -0500

    Tests Organization

--- src/main/java/com/ferroblesh/javatesting/util/StringUtil.java
+++ src/main/java/com/ferroblesh/javatesting/util/StringUtil.java
@@ -5,6 +5,9 @@ public class StringUtil {
     public static String repeat(String str, int times) {
         String result = "";
 
+        if(times < 0)
+            throw new IllegalArgumentException("Ilegal arguments");
+
         for (int i = 0; i < times; i++) {
             result += str;
         }
--- src/test/java/com/ferroblesh/javatesting/util/StringUtilTest.java
+++ src/test/java/com/ferroblesh/javatesting/util/StringUtilTest.java
@@ -8,10 +8,30 @@ import static org.junit.Assert.assertEquals;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat(){
+    public void repeatStringOnce(){
+
+        assertEquals("hola" , StringUtil.repeat("hola", 1));
+
+    }
+
+    @Test
+    public void repeatStringMultipleTimes(){
 
         assertEquals("holaholahola",StringUtil.repeat("hola", 3));
-        assertEquals("hola" , StringUtil.repeat("hola", 1));
+
+    }
+
+    @Test
+    public void repeatStringZeroTimes(){
+
+        assertEquals("",StringUtil.repeat("hola", 0));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeatStringNegativeTimes(){
+
+        assertEquals("",StringUtil.repeat("hola", -1));
 
     }
 
